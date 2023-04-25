9494f72833b8d37d0e1dc83e291dd67cd7ae64eb
manuzagra <manuzagra@gmail.com>
2023-04-25 03:03:36 +0200

    add difficult and otrher things

--- bot.py
+++ bot.py
@@ -18,7 +18,7 @@ MAIN_OPTIONS, INTRO_NUMBER_SAVE_NAME_REQ_NUMBER, INTRO_NUMBER_SAVE_NUMBER, GUESS
 PARTICIPANTS = ['Manuel', 'Alfonsito', 'Carlos Ropero', 'Xulo']
 
 # texts
-INTRO_NUMBERS = 'Introducir un numero/letra que has conseguido'
+INTRO_NUMBERS = 'Introducir un número/letra que has conseguido'
 GUESS_GAME = 'Jugar para adivinar el orden'
 
 
@@ -30,7 +30,7 @@ async def init(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
         if not context.user_data.get('numbers', False):
             context.user_data['numbers'] = {p: '-' for p in PARTICIPANTS}
             context.user_data['numbers'] = {'Manuel': '1', 'Alfonsito': '2', 'Carlos Ropero': '3', 'Xulo': '4'}
-        message = 'Bienvenido al juego de tu boda!!!\nPrimero tendras que conseguir los números/letras de los participantes en el juego, depués tendras que jugar a un juego para averiguar el orden.\n¿Qué quieres hacer?'
+        message = 'Bienvenido al juego de tu boda!!!\nPrimero tendrás que conseguir una serie de carácteres de los participantes en el juego, después tendrás que jugar a un juego para averiguar el orden.\n¿Qué quieres hacer?'
         context.user_data['new_interaction'] = False
     else:
         message = '¿Qué quieres hacer ahora?'
@@ -54,7 +54,7 @@ async def intro_number_req_name(update: Update, context: ContextTypes.DEFAULT_TY
     markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True)
 
     await update.message.reply_text(
-        '¿De quien has conseguido el número?',
+        '¿De quién has conseguido el carácter?',
         reply_markup=markup,
     )
     return INTRO_NUMBER_SAVE_NAME_REQ_NUMBER
@@ -63,7 +63,7 @@ async def intro_number_save_name_req_number(update: Update, context: ContextType
     context.user_data['numbers'][update.message.text] = -1
     context.user_data['last_person'] = update.message.text
 
-    await update.message.reply_text('¿Qué número te ha dicho?')
+    await update.message.reply_text('¿Qué carácter te ha dicho?')
 
     return INTRO_NUMBER_SAVE_NUMBER
 
@@ -78,7 +78,7 @@ async def guess_number_intro_number(update: Update, context: ContextTypes.DEFAUL
 
     digits = ''.join(context.user_data['numbers'].values())
 
-    await update.message.reply_text(f'Los número/letras guardados son los siguientes:\n{digits}\nEscríbelos en el orden que creas correcto.')
+    await update.message.reply_text(f'Los caracteres guardados son los siguientes:\n{digits}\nEscríbelos en el orden que creas correcto.')
 
     return GUESS_GAME_REQ_ORDER
     
@@ -87,22 +87,61 @@ async def guess_number_check_number(update: Update, context: ContextTypes.DEFAUL
 
     truth = '1234'
 
-    message = ''
-    for i in range(min(len(update.message.text), len(truth))):
-        message += 'o' if update.message.text[i].upper() == truth[i] else 'x'
+    digits = ''.join(context.user_data['numbers'].values())
 
-    if 'x' in message:
-        message = str(update.message.text) + '\n' + message + '\n¿Quieres intentarlo otra vez (si/no)?'
+    for d in update.message.text:
+        if d not in digits:
+            message = 'Hay uno o más carácteres incorrectos, por favor usa solo los carácteres que te ha dado la gente.\n¿Quieres intentarlo otra vez ( /si o /no)?'
 
-        await update.message.reply_text(message)
+            await update.message.reply_text(message)
 
-        return GUESS_GAME_INTRO_NUMBER
+            return GUESS_GAME_INTRO_NUMBER
+
+    easy_mode = False
+
+    if easy_mode:
+
+        message = ''
+        for i in range(min(len(update.message.text), len(truth))):
+            message += 'o' if update.message.text[i].upper() == truth[i] else 'x'
+
+        if 'x' in message:
+            message = str(update.message.text) + '\n' + message + '\n¿Quieres intentarlo otra vez ( /si o /no)?'
+
+            await update.message.reply_text(message)
+
+            return GUESS_GAME_INTRO_NUMBER
+        else:
+            message = str(update.message.text) + '\n' + message + '\nCORRECTO!!!\nYa tienes el código!!! Escribe /salir para finalizar o /instrucciones para obtener instrucciones de que hacer con el código que acabas de conseguir.'
+
+            await update.message.reply_text(message)
+
+            return END
     else:
-        message = str(update.message.text) + '\n' + message + '\nCORRECTO!!!\nYa tienes el código!!! Escribe /salir finalizar o /instrucciones para obtener instrucciones de que hacer con el código.'
+        correct_position = 0
+        for i in range(min(len(update.message.text), len(truth))):
+            correct_position += 1 * (update.message.text[i].upper() == truth[i])
 
-        await update.message.reply_text(message)
+        if correct_position < len(truth):
+            message = f'{correct_position} carácteres están en la posicion correcta.\n¿Quieres intentarlo otra vez ( /si o /no)?'
 
-        return END
+            await update.message.reply_text(message)
+
+            return GUESS_GAME_INTRO_NUMBER
+        else:
+            message = '\nCORRECTO!!!\nYa tienes el código!!! Escribe /salir para finalizar o /instrucciones para obtener instrucciones de que hacer con el código que acabas de conseguir.'
+
+            await update.message.reply_text(message)
+
+            return END
+
+async def instructions(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+
+    INSTRUCTIONS = 'Here you would see the instructions'
+
+    await update.message.reply_text(INSTRUCTIONS + '\n\nPara volver a comenzar escribe /start.')
+
+    return ConversationHandler.END
 
 async def end(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
     logger.debug(context.user_data['numbers'])
@@ -126,7 +165,7 @@ def get_state_machine():
             INTRO_NUMBER_SAVE_NUMBER: [MessageHandler(filters.TEXT, intro_number_save_number)],
             GUESS_GAME_INTRO_NUMBER: [MessageHandler(filters.Regex(f'^No|no$'), init), MessageHandler(filters.TEXT & ~filters.Regex(f'^No|no$'), guess_number_intro_number)],
             GUESS_GAME_REQ_ORDER: [MessageHandler(filters.TEXT, guess_number_check_number)],
-            END: [MessageHandler(filters.TEXT, init), CommandHandler('menu', init), CommandHandler('salir', end)],
+            END: [CommandHandler('menu', init), CommandHandler('salir', end), CommandHandler('instrucciones', instructions), MessageHandler(filters.TEXT, init)],
         },
         fallbacks=[MessageHandler(filters.Regex('^[Ss]alir$'), end), MessageHandler(filters.Regex('^[Mm]enu|[Ss]tart$'), init)],
     )
@@ -139,7 +178,7 @@ if __name__ == '__main__':
     TOKEN = '6087048986:AAFSoEq1zJefaTgVv3sgUK6jAFYZOCLSu0Y'
 
     persistence = PicklePersistence(filepath='/workspaces/get_the_password_bot/persistent_data/bot_data.pkl')
-    application = ApplicationBuilder().token(TOKEN).persistence(persistence).build()
+    application = ApplicationBuilder().token('6087048986:AAFSoEq1zJefaTgVv3sgUK6jAFYZOCLSu0Y').persistence(persistence).build()
 
     application.add_handler(get_state_machine())
 
